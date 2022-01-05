90cf7d1a3cf13431e57618e49e21995b114e29ab
Michael Wasilewski <michal.wasilewski@stud.noroff.no>
2022-01-05 09:02:11 +0100

    learning about strings,boolean typeof

---
+++ script.js
@@ -0,0 +1,52 @@
+var animal = "frog"; //frog
+var delivered = "true"; //
+var score = 12;
+var fullName ="John" + "Smith";
+
+
+
+var petOne = "rab";
+var petTwo = "bit";
+
+var myPets = "my pets are:"  + petOne + petTwo;
+console.log(myPets); // my pets are: cat dog
+
+var four = "4";
+console.log(four);
+
+var myFirstNumber = "4";
+var mySecondNumber = 4;
+
+var sumOfMyNumbers = myFirstNumber + mySecondNumber;
+console.log("addition:",sumOfMyNumbers); // 44 without "" it will show 8
+
+var integer = 8;
+var decimal = 10.1;
+
+var four = 4;
+
+console.log(four);
+
+var multiOfMyNumbers = myFirstNumber * mySecondNumber;
+console.log(multiOfMyNumbers);
+
+var divisionOfMyNumbers = myFirstNumber / mySecondNumber;
+console.log(divisionOfMyNumbers);
+
+
+var myThridNumber = 70;
+var myFourthNumber = 87;
+
+var sumOfMyNumbers = myThridNumber + myFourthNumber;
+
+console.log(sumOfMyNumbers);
+
+var lightIsOn = false;
+console.log(lightIsOn);
+
+var color = "red";
+typeof(color);
+
+var number = 23;
+typeof(number);
+console.log(23);
